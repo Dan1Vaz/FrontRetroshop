7f72a1c49db147281a4927638a3bebea24c580bb
Miguel Garcia <migueldariogq.mdg@gmail.com>
2024-01-17 18:37:04 +0100

    cosas

--- retroShopFront/src/components/Navbar.jsx
+++ retroShopFront/src/components/Navbar.jsx
@@ -1,12 +1,19 @@
+import SearchIcon from "@mui/icons-material/Search";
+
 export const Navbar = () => {
   return (
-    <div className="Navbar flex flex-row justify-center pt-4 gap-[20px]">
-      <img src="/iconretroshop.svg" alt="" className="w-[35px] h-[53px]" />
-      <input
-        className="w-[278px] h-[33] bg-[#efefef] rounded-full p-[20px] "
-        type="search"
-        placeholder="Busca un producto"
-      />
+    <div className="Navbar flex flex-row justify-center items-center pt-4 gap-[20px] relative">
+      <img src="/iconretroshop.svg" alt="" className="w-[px] h-[53px]" />
+      <div className="SearchBar relative flex items-center"> 
+        <input
+          className="w-[278px] h-[33px] bg-[#efefef] rounded-full p-[20px] "
+          type="search"
+          placeholder="Busca un producto..."
+        />
+        <div className="flex absolute justify-center items-center right-[5px] top-[5px] w-[28px] h-[28px] bg-white rounded-full">
+          <SearchIcon className=" fill-[#FD2A5C] " />
+        </div>
+      </div>
     </div>
   );
 };
