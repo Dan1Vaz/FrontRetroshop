efb9980109f3cc1233d8e12bed99dc32946cafec
marta <105271643+ffanecaa@users.noreply.github.com>
2024-02-02 17:18:39 +0100

    video 404

--- retroShopFront/src/pages/Error404.jsx
+++ retroShopFront/src/pages/Error404.jsx
@@ -5,7 +5,7 @@ const Error404 = () => {
   return (
     <div className="container">
     <video  autoPlay loop muted  className="video">
-        <source src="retrol.mp4" type="video/mp4"/>
+        <source src="retro.mp4" type="video/mp4"/>
         <Link className="video_link"to={`${import.meta.env.VITE_BASE_URL}:5173`}> volver</Link>
     </video>
     </div>
