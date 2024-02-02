cb2d2c18d10694ccd598a9b61868cc8f9bfa591e
git config --global user.name <dani.vazquez.gonzalez@gmail.com>
2024-02-02 19:59:25 +0100

    Nombre usuario en perfil y avatar

--- retroShopFront/src/pages/ProfilePage.jsx
+++ retroShopFront/src/pages/ProfilePage.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { authContext } from "../providers/AuthProvider";
 import { Navigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom"
+import useGetUser from "../Hook/useGetUser";
 
 export const ProfilePage = () => {
   const [token] = useContext(authContext);
@@ -9,32 +10,35 @@ export const ProfilePage = () => {
   const btnNav = ()=>{navigate("/profile/reservations")};
   const btnNav2 = ()=>{navigate("/profile/products/user")};
   const btnNav3 = ()=>{navigate("/profile/perfil")};
+ const {data,loading, error} = useGetUser()
+
+console.log(data);
 
   if (!token) {
     return <Navigate to="/profile/login" />;
   }
   return (
-    <div className="flex b items-center flex-col gap-[20px] h-screen bg-[#F6F6F6]">
-      <div className="w-[424px] h-[160px] bg-white">
+    <div className="flex b items-center flex-col gap-[20px] bg-[#F6F6F6]">
+      <div className="w-[375px] h-[160px] bg-white">
         <img
-          src="/iconProfileEmpty.svg"
+          src={`http://${import.meta.env.VITE_BASE_URL}:3001/${data.avatarURL}`}
           alt=""
-          className="w-[73px] h-[73px] relative top-[74px] left-[46px]"
+          className="w-[73px] h-[73px] relative top-[74px] left-[26px]"
         />
         <h1 className="w-[87px] h-[21px] top-[25px] left-[145px] relative font-family Roboto font-size 15px font-weight 500 line-height 26px text-transform uppercase">
-          USER NAME
+         {data.name}
         </h1>
       </div>
 
-      <div className="pt-100px w-[424px] h-[104px] bg-white mt-[80px]">
+      <div className="pt-50px w-[375px] h-[104px] bg-white mt-[30px]">
         <h2 className="relative top-[-25px] left-[18px] text-lg">
           Transacciones
         </h2>
 
-        <div className="w-[424px] h-[40px] d-flex flex-row">
+        <div className="w-[375px] h-[40px] d-flex flex-row">
           <button
             onClick={btnNav}
-            className="w-[424px] h-[40px] flex flex-row justify-start pb-[6px]"
+            className="w-[375px] h-[40px] flex flex-row justify-start pb-[6px]"
           >
             <img
               src="/handshake.svg"
@@ -70,23 +74,28 @@ export const ProfilePage = () => {
         </div>
       </div>
 
-      <div className="pt-100px w-[424px] h-[60px] bg-white mt-[80px] d-flex flex-row">
-        <button 
-        type="button"
-        onClick={btnNav3}
-        className="w-[424px] h-[40px] flex column pt-[6px]">
-          <img
-            src="/settings.svg"
-            alt=""
-            className="object-scale-down w-[32] h-[20] pl-[12px] pr-[6px]"
-          />
-          <h2 className="inline pl-[6px] text-lg pt-[6px]">Configuración</h2>
-          <img
-            src="/vector.svg"
-            alt=""
-            className="inline ml-auto w-[12px] h-[20px]"
-          />
-        </button>
+      <div className="w-[375px] h-[60px] bg-white mt-[30px]">
+        <h2 className="relative top-[-25px] left-[18px] text-lg">
+          Cuenta
+        </h2>
+        <div className="w-[375px] h-[60px] bg-white d-flex flex-row">
+          <button 
+          type="button"
+          onClick={btnNav3}
+          className="w-[375px] h-[40px] flex column pt-[6px]">
+            <img
+              src="/settings.svg"
+              alt=""
+              className="object-scale-down w-[32] h-[20] pl-[12px] pr-[6px]"
+            />
+            <h2 className="inline pl-[6px] text-lg pt-[6px]">Configuración</h2>
+            <img
+              src="/vector.svg"
+              alt=""
+              className="inline ml-auto w-[12px] h-[20px]"
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
