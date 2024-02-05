2dbd93fde8a4379e31cad5aaf95f23d80bba4270
LuisManuel Dominguez <lmadb1991@gmail.com>
2024-02-05 18:21:18 +0100

    cleaning ccs from ReservationPage

--- retroShopFront/src/components/ReservBlank.jsx
+++ retroShopFront/src/components/ReservBlank.jsx
@@ -1,6 +1,6 @@
 export const ReservBlank = () => {
   return (
-    <div className="flex flex-col justify-center items-center pt-4 gap-[20px] relative mx-10 mt-10">
+    <div className="flex flex-col justify-center items-center gap-[20px] relative mx-10 my-10">
       <img src="/iconretroshop.svg" alt="" className="size-24" />
       <h1 className="text-2xl font-bold">Aun no hay reservas</h1>
       <p className="text-xl font-semibold text-gray-700 text-center px-5">Aquí podrás recibir actualizaciones sobre el estado de tu reserva después de que reserves algo</p>
