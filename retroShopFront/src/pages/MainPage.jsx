ea3cc8c38d755f3e61f8812687ebda8f27b3d246
git config --global user.name <dani.vazquez.gonzalez@gmail.com>
2024-02-08 18:05:11 +0100

    videoMainPage

--- retroShopFront/src/pages/MainPage.jsx
+++ retroShopFront/src/pages/MainPage.jsx
@@ -1,19 +1,18 @@
 import { ProductCard } from "../components/ProductCard/ProductCard";
 import { Link } from "react-router-dom";
 import useProducts from "../Hook/useProducts";
+import VideoMain from "../components/VideoMainPage";
 
 export const MainPage = () => {
   const { products, loading, error } = useProducts(20);
   //console.log(products);
   return (
     <>
-      <header className="flex h-[135px] justify-center items-center">
-        <h1 className="font-bold text-[#080C89] pl-2">
-          COMPRA Y VENDE LOS MEJORES PRODUCTOS RETRO EN NUESTRA PÁGINA
-        </h1>
+      <header className="flex h-[135px] w-full justify-center items-center">
+        <VideoMain></VideoMain>
       </header>
       <main className="pb-28">
-        <div className="flex items-center justify-center border-y-2 border-[#7C7C7C] h-[76px]">
+        <div className="flex items-center justify-center border-y-2 border-[#7C7C7C] h-[76px] mt-8">
           <h2 className="font-bold text-[#080C89] ">PRODUCTOS DESTACADOS</h2>
         </div>
         <ul className="productsTable grid grid-cols-2 gap-4 justify-items-center w-full pt-[15px]">
