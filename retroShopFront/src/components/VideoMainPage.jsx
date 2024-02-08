82a3a550253995124dbe6241cd966a6351d3ada3
git config --global user.name <dani.vazquez.gonzalez@gmail.com>
2024-02-08 18:14:06 +0100

    autoPlay

--- retroShopFront/src/components/VideoMainPage.jsx
+++ retroShopFront/src/components/VideoMainPage.jsx
@@ -1,6 +1,6 @@
 export default function VideoMain() {
     return (
-      <video className=" w-full" loop muted>
+      <video className=" w-full" autoPlay loop muted>
         <source src="./videoMainPage.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
