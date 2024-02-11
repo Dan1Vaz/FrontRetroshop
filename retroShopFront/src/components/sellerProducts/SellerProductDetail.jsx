930a276ccc13df0f0406e8fdfd14cc75396ed279
LuisManuel Dominguez <lmadb1991@gmail.com>
2024-02-11 22:55:27 +0100

    sellerdetail fixed

--- retroShopFront/src/components/sellerProducts/SellerProductDetail.jsx
+++ retroShopFront/src/components/sellerProducts/SellerProductDetail.jsx
@@ -13,9 +13,8 @@ import DeletedReservation from "../DeletedReservation.jsx";
 
 const SellerProductDetail = ({ data }) => {
   return (
-    <div className="flex justify-center">
-    <div className="flex flex-col pb-28 lg:max-w-[700px] lg:border-2 lg:rounded-lg lg:overflow-hidden lg:shadow-lg lg:pb-6  lg:text-center justify-center">
-      <div className="w-full relative ">
+    <div className="flex flex-col pb-28 lg:max-w-[700px] lg:border-2 lg:rounded-lg lg:overflow-hidden lg:shadow-lg lg:pb-6  lg:text-center justify-center lg:ml-auto lg:mr-auto">
+      <div className="w-full relative">
         <DetailedProductImage
           productImg1={data.product.imageURL}
           productImg2={data.product.imageURL2}
@@ -78,7 +77,6 @@ const SellerProductDetail = ({ data }) => {
           </>
         ) : null}
       </div>
-    </div>
     </div>
   );
 };
