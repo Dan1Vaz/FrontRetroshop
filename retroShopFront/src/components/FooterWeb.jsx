3b4f17205cddba4eafd9533cb2e6539c5567e7f6
marta <105271643+ffanecaa@users.noreply.github.com>
2024-02-11 12:11:25 +0100

    footer

--- retroShopFront/src/components/FooterWeb.jsx
+++ retroShopFront/src/components/FooterWeb.jsx
@@ -1,18 +1,39 @@
 import { Link } from "react-router-dom"
 import Contact from "./Contact"
-
+import LaptopChromebookIcon from '@mui/icons-material/LaptopChromebook';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import RadioIcon from '@mui/icons-material/Radio';
+import DevicesOtherIcon from '@mui/icons-material/DevicesOther';
+import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
 
 export const FooterWeb = () => {
   return (
-    <div className="hidden lg:flex flex-col bg-gray-200 text-white max-w-screen shadow-[9px_-2px_18px_3px_rgba(133,126,133,1)] ">
+    <div className="hidden lg:flex flex-col bg-gray-200 text-grey-700 max-w-screen shadow-[9px_-2px_18px_3px_rgba(133,126,133,1)] ">
      ;
-        <div className=" flex flex-row text-center justify-center p-[20px] " >
+        <div className=" flex flex-row text-center justify-center px-[10px] " >
         
-           <div className=" flex flex-col w-[30vw] gap-10 text-justify text-sm">
-            <h2 className="text-base font-semibold text-gray-700">Links</h2>
-            <span>Consola</span>
-            <span>ordenas</span>
-            <span>otros</span>
+           <div className=" flex flex-col w-[30vw] gap-10 justify-center items-center  text-md">
+           <div className="grid grid-cols-2 gap-4 text-center">
+            <Link to="/products?category=Consola">
+            <p className="mx-3 text-grey-700  text-shadow-md"><SportsEsportsIcon/> Consolas</p>
+          </Link>
+
+          <Link to="/products?category=Videojuego">
+            <p className="mx-3  "> <VideogameAssetIcon/> Videojuegos</p>
+          </Link>
+          <a href="" className="mx-3  text-grey-700 ">
+           <RadioIcon/> Radios
+          </a>
+          <a href="" className="mx-3 text-grey-700 ">
+           <DevicesOtherIcon/> Televisores
+          </a>
+          <Link to="/products?category=Ordenador">
+            <p className="mx-3 text-grey-700 "> <LaptopChromebookIcon/> Ordenadores</p>
+          </Link>
+          <a href="" className="mx-3  text-grey-700 ">
+           <PhoneIphoneIcon/> Móviles
+          </a></div>
            </div>
            <div className=" flex flex-col w-[30vw]  text-justify p-5 text-sm">
            <div className=" flex  justify-center text-center  ">
@@ -35,7 +56,7 @@ export const FooterWeb = () => {
         <div className=" w-[50%] flex gap-5 text-center  justify-center mt-2">
         <span className=" text-sm text-gray-500">© copyright derechos reservados</span>
             <span className=" text-sm text-gray-500">Política de privacidad</span>
-           <Link className=" text-sm text-gray-500" to="/profile/cookies">Política de cookies</Link>
+           <Link className=" text-sm text-gray-500"to="/profile/cookies">Política de cookies</Link>
         </div>
     </div>
 </div>
